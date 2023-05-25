bf9c07b047646d0db520ba26c406afe3ef0f9995
theisolatedmonk <96rahulseth@gmail.com>
2023-05-25 21:40:26 +0530

    type-assigned

--- src/app/AllApp.tsx
+++ src/app/AllApp.tsx
@@ -11,15 +11,24 @@ import PasswordGenerator from './PasswordGenerator';
 import Question from './Question';
 
 
-export default function AllApp() {
-  const [activeComponent, setActiveComponent] = useState(null);
+// export default function AllApp() {
+//   const [activeComponent, setActiveComponent] = useState(null);
 
-  const handleComponentChange = (componentName) => {
-    setActiveComponent(componentName );
-  };
+//   const handleComponentChange = (componentName) => {
+//     setActiveComponent(componentName );
+//   };
 
-  return (
-    <div className="flex flex-col gap-4 items-center min-h-screen min-w-screen py-2 justify-center text-black bg-green-300 flex-wrap">
+  // import React, { useState } from 'react';
+
+  const AllApp = () => {
+    const [activeComponent, setActiveComponent] = useState<string | null>(null);
+  
+    const handleComponentChange = (componentName: string) => {
+      setActiveComponent(componentName);
+    };
+  
+    return (
+      <div className="flex flex-col gap-4 items-center min-h-screen min-w-screen py-2 justify-center text-black bg-green-300 flex-wrap">
       <div className="flex gap-4">
         <button
           className="bg-yellow-400 p-2 rounded-lg"
@@ -51,5 +60,44 @@ export default function AllApp() {
       {activeComponent === 'generator' && <PasswordGenerator />}
       {activeComponent === 'question' && <Question/>}
     </div>
-  );
-}
+    );
+  };
+  
+  export default AllApp;
+  
+  
+//   return (
+//     <div className="flex flex-col gap-4 items-center min-h-screen min-w-screen py-2 justify-center text-black bg-green-300 flex-wrap">
+//       <div className="flex gap-4">
+//         <button
+//           className="bg-yellow-400 p-2 rounded-lg"
+//           onClick={() => handleComponentChange('encryption')}
+//         >
+//           Data Encryption and Decryption
+//         </button>
+//         <button
+//           className="bg-yellow-400 p-2 rounded-lg"
+//           onClick={() => handleComponentChange('analyser')}
+//         >
+//           Password Analyzer
+//         </button>
+//         <button
+//           className="bg-yellow-400 p-2 rounded-lg"
+//           onClick={() => handleComponentChange('generator')}
+//         >
+//           Password Generator
+//         </button>
+//         <button
+//           className="bg-yellow-400 p-2 rounded-lg"
+//           onClick={() => handleComponentChange('question')}
+//         >
+//           Question Answer
+//         </button>
+//       </div>
+//       {activeComponent === 'encryption' && <Encryption />}
+//       {activeComponent === 'analyser' && <PasswordAnalyser />}
+//       {activeComponent === 'generator' && <PasswordGenerator />}
+//       {activeComponent === 'question' && <Question/>}
+//     </div>
+//   );
+// }
