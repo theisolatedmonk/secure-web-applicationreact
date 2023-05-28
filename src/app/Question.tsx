b0003b8d5880c1b379ecde5ac7e40b30de7331f8
theisolatedmonk <96rahulseth@gmail.com>
2023-05-28 17:13:53 +0530

    more-questions-added

--- src/app/Question.tsx
+++ src/app/Question.tsx
@@ -15,34 +15,54 @@ const questions: Question[] = [
   },
   { id: 2, text: "2. Do you change your passwords regularly?" },
   {
-    id: 3,
-    text: "3. Do you use a password manager to help you keep track of your passwords?",
+    id: 3, text: "3. Do you keep your software up to date?",  
   },
   {
     id: 4,
     text: "4. Do you avoid using personal information in your online usernames?",
   },
   { id: 5, text: "5. Do you only use secure websites?" },
-  //   { id: 6, text: '6. Do you keep your software up to date?' },
-  //   { id: 7, text: '7. Do you use a firewall and antivirus software?' },
-  //   { id: 8, text: '8. Are you careful about what information you share online?' },
-  //   { id: 9, text: '9. Do you know how to report a security breach?' },
-  //   { id: 10, text: '10. Do you know the signs that your identity may have been stolen?' },
-  //   { id: 11, text: '11. Do you know what to do if you think your identity has been stolen?' },
-  //   { id: 12, text: '12. Do you use strong passwords for your social media accounts?' },
-  //   { id: 13, text: '13. Do you avoid sharing your full name, date of birth, or home address on social media?' },
-  //   { id: 14, text: '14. Are you careful about what photos and videos you share on social media?' },
-  //   { id: 15, text: '15. Do you friend or follow strangers on social media?' },
-  //   { id: 16, text: '16. Are your social media privacy settings tight?' },
-  //   { id: 17, text: '17. Do you report any suspicious activity to the social media platform?' },
-  //   { id: 18, text: '18. Do you have a plan in place in case your identity is stolen?' },
-  //   { id: 19, text: '19. Are you aware of the latest scams and phishing attacks?' },
-  //   { id: 20, text: '20. Do you enable two-factor authentication for your online accounts whenever possible?' },
-  //   { id: 21, text: '21. Do you regularly review your online account activity and check for any unauthorized access or suspicious transactions?' },
-  //   { id: 22, text: '22. Do you avoid clicking on suspicious links or opening email attachments from unknown sources?' },
-  //   { id: 23, text: '23. Do you use different passwords for each of your online accounts?' },
-  //   { id: 24, text: '24. Do you regularly backup important data and files to prevent loss in case of a security breach or device failure?' },
-  //   { id: 25, text: '25. Do you educate yourself about common cybersecurity threats and stay updated on best practices for online safety?' },
+
+  { id: 6, text: "6. Do you know how to report a security breach?" },
+  {
+    id: 7,
+    text: "7. Do you know what to do if you think your identity has been stolen?",
+  },
+  
+  {
+    id: 8,
+    text: "8. Do you avoid sharing your full name, date of birth, or home address on social media?",
+  },
+ 
+  { id: 9, text: "9. Do you friend or follow strangers on social media?" },
+  {
+    id: 10,
+    text: "10. Do you report any suspicious activity to the social media platform?",
+  },
+  {
+    id: 11,
+    text: "11. Are you aware of the latest scams and phishing attacks?",
+  },
+  {
+    id: 12,
+    text: "12. Do you enable two-factor authentication for your online accounts whenever possible?",
+  },
+ 
+
+
+  {
+    id: 13,
+    text: "13. Do you avoid clicking on suspicious links or opening email attachments from unknown sources?",
+  },
+  {
+    id: 14,
+    text: "14. Do you use different passwords for each of your online accounts?",
+  },
+  {
+    id: 15,
+    text: "15. Do you regularly backup important data and files to prevent loss in case of a security breach or device failure?",
+  },
+
   // Add the remaining questions here
 ];
 
@@ -90,11 +110,11 @@ const Question: React.FC = () => {
   const isSubmitDisabled = Object.keys(answers).length !== questions.length;
 
   return (
-    <div className="container mx-auto">
+    <div className="container  p-8">
       <h1 className="text-4xl font-bold my-4">Online Security Assessment</h1>
-      <form onSubmit={handleSubmit}>
+      <form className="" onSubmit={handleSubmit}>
         {questions.map((question) => (
-          <div key={question.id} className="my-4">
+          <div key={question.id} className=" flex flex-col gap-2 ">
             <h3 className="text-lg font-semibold">{question.text}</h3>
             <div className="flex items-center">
               <input
@@ -130,10 +150,15 @@ const Question: React.FC = () => {
       </form>
       {score > 0 && (
         <div className="mt-4">
-          <h2 className="text-2xl font-bold">Your Score: {score}%</h2>
-          <p className="mt-2">
+          <h2 className="text-2xl font-bold">Your Score: {0 + score}%</h2>
+          <p className="mt-2 w-[70%] text-white">
             {getMessage()}{" "}
-            <Link href="https://dot.gov.in/banner/cyber-dost"><span className="bg-gray-800 p-1 rounded-3xl text-white">Click here</span>  to learn about Cyber Security tips</Link>
+            <Link href="https://dot.gov.in/banner/cyber-dost">
+              <span className="bg-white p-1 rounded-3xl text-gray-800">
+                Click here
+              </span>{" "}
+              to learn about Cyber Security tips
+            </Link>
           </p>
         </div>
       )}
